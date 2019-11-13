b8fbd7a1ea9926f1d0f45ac6b577e84438beda12
martin9518 <57213162+martin9518@users.noreply.github.com>
2019-11-13 18:52:56 +0100

    zadanie4_2

--- javascript.js
+++ javascript.js
@@ -1,30 +1,43 @@
-class Animal {
-    typ=null;
-     constructor(typ, zvuk){ 
-        this.typ = typ;
-        this.zvuk = zvuk;
-}
-speak(){
-    alert(this.typ + "makes zvuk" + this.zvuk)
-}
+const Map =ol.Map; 
+const View = ol.View; 
+const ImageLayer= ol.layer.Image; 
+const TileLayer=ol.layer.Tile;
+const ImageWMS=ol.source.ImageWMS;
+const OSM =ol.source.OSM ;
 
-}
-const pes = new Animal("pes", "štekat");
-const macka = new Animal ("mačka"," mnaukat ");
+var layers = [
+  new TileLayer({
+    
+    source: new OSM()
+  }),
+  new ImageLayer({
+    extent: [17.84506885869866, 48.731926770588124, 17.93695929993104, 48.764736748435276],
+    source: new ImageWMS({
+      url: 'http://localhost:8080/geoserver/ows?',
+      params: {LAYERS: ['cviko:budovy02', 'cviko:cesta','cviko:el_stlpy','cviko:el_vedenie','cviko:ihrisko','cviko:luka']},
+      ratio: 1,
+      serverType: 'geoserver'
+    }),
+  })
+];
 
-pes.speak();
-macka.speak()
+var map = new Map({
+ layers: layers,
+  target: 'map',
+  view: new View({
+    projection: 'EPSG:4326',
+    center: [17.87, 48.75],
+    zoom: 13
+  })
+});
 
-class pes extends Animal {
-    typpsa =null;
-    constructor(typpsa){
-        super("pes","haf");
-        this.typpsa=typpsa;
-    }
-speak(){
-    alert(this.typ+""+ this.typpsa+"robi zvuk"+ this.zvuk);
-}
-}
-const bigl = new pes ("bigl");
-bigl.speak();
-
+function funkcia() {
+  fetch('http://localhost:8080/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities',{mode:'cors'}).then(function(response) {
+    return response.text();
+  }).then(function(data) {
+    let parser = new DOMParser();
+    XMLDocument=parser.parseFromString(data,'text/xml');
+    console.log(XMLDocument.getElementsByTagName('sco'));
+    document.getElementById('Capabilities').textContent=data;
+  });
+  }
